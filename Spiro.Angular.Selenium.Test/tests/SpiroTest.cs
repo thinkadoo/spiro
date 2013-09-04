c0eb2d7e60245e9c485e65d2ad090c3b59d94c3d
scascarini <scascarini@nakedobjects.org>
2013-09-04 15:58:59 +0100

    add end to end test project

---
+++ Spiro.Angular.Selenium.Test/tests/SpiroTest.cs
@@ -0,0 +1,97 @@
+﻿// Copyright © Naked Objects Group Ltd ( http://www.nakedobjects.net). 
+// All Rights Reserved. This code released under the terms of the 
+// Microsoft Public License (MS-PL) ( http://opensource.org/licenses/ms-pl.html) 
+
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NakedObjects.Xat.Database;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NakedObjects.Web.UnitTests.Selenium {
+    [TestClass]
+    public abstract class SpiroTest {
+        #region overhead
+
+        protected const string url = "http://mvc.nakedobjects.net:1081/UnitTestSpiroNg";
+        protected const string server = @"Saturn\SqlExpress";
+        protected const string database = "AdventureWorks";
+        protected const string backup = "AdventureWorks";
+
+        //protected const string url = "http://localhost:53103/";
+        //protected const string server = @".\SQLEXPRESS";
+        //protected const string database = "AdventureWorks";
+        //protected const string backup = "AdventureWorksInitialState";
+
+        protected IWebDriver br;
+
+        [ClassInitialize]
+        public static void InitialiseClass(TestContext context) {
+            DatabaseUtils.RestoreDatabase(database, backup, server);
+        }
+
+        public virtual void CleanUpTest() {
+            if (br != null) {
+                try {
+                    br.Manage().Cookies.DeleteAllCookies();
+                    br.Quit();
+                    br.Dispose();
+                    br = null;
+                }
+                catch {
+                    // to suppress error 
+                }
+            }
+        }
+
+        protected IWebDriver InitChromeDriver() {
+            const string cacheDir = @"C:\SeleniumTestFolder";
+
+            var crOptions = new ChromeOptions();
+            crOptions.AddArgument(@"--user-data-dir=" + cacheDir);
+            var cd = new ChromeDriver(crOptions);
+
+            // test workaround for chromedriver problem https://groups.google.com/forum/#!topic/selenium-users/nJ0NF1UJ3WU
+            Thread.Sleep(5000);
+            return cd;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        protected void Login() {
+            Thread.Sleep(2000);
+        }
+
+        #endregion
+
+        #region chrome helper
+
+        protected static string FilePath(string resourcename) {
+            string fileName = resourcename.Remove(0, resourcename.IndexOf(".") + 1);
+
+            string newFile = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+            if (File.Exists(newFile)) {
+                File.Delete(newFile);
+            }
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            using (Stream stream = assembly.GetManifestResourceStream("Spiro.Angular.Selenium.Test." + resourcename)) {
+                using (FileStream fileStream = File.Create(newFile, (int) stream.Length)) {
+                    var bytesInStream = new byte[stream.Length];
+                    stream.Read(bytesInStream, 0, bytesInStream.Length);
+                    fileStream.Write(bytesInStream, 0, bytesInStream.Length);
+                }
+            }
+
+            return newFile;
+        }
+
+        #endregion
+    }
+}
